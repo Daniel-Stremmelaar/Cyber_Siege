d2905debc380842550717c8f4844f1722cb81e98
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2019-08-30 15:43:01 +0200

    Main menu
    Main menu basic scene and start of code

---
+++ Cyber_Siege/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public List<Button> sceneButtons = new List<Button>();
+    public List<int> sceneIndexes = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
