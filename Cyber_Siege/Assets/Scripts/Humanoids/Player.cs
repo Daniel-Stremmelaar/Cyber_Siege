690ee5d5d3fe5eb0695f7963ba776a18fd51d28e
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2019-08-30 15:43:49 +0200

    Mechanim Test
    +Added my own scene
    +Added really really really basic movement (Camera not included yet)
    +Added mechanim blend trees for movement

---
+++ Cyber_Siege/Assets/Scripts/Humanoids/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Transform playerCamera;
+    [SerializeField] Animator playerAnimator;
+    [SerializeField] float movementSpeedModifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementAmount = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        playerAnimator.SetFloat("SidewaysWalking", movementAmount.x);
+        playerAnimator.SetFloat("ForwardWalking", movementAmount.z);
+        if(movementAmount != Vector3.zero)
+        {
+            transform.Translate(movementAmount * movementSpeedModifier * Time.deltaTime);
+        }
+    }
+}
