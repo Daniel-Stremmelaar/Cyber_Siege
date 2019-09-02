1455f4cf13fe1ef37a16a453ef25c268b2173022
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2019-09-02 10:35:55 +0200

    Basic Controls & Parkour Scene
    Basic controls for movement and shooting work. A simple parkour course has been made. No special movement yet.

---
+++ Cyber_Siege/Assets/Scripts/LookUpDown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookUpDown : MonoBehaviour {
+    private Vector3 r;
+    public float rotSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //rotation look
+        r.x += -Input.GetAxis("Mouse Y") * rotSpeed;
+        r.x = Mathf.Clamp(r.x, -50.0f, 50.0f);
+        transform.eulerAngles = (new Vector3(r.x, transform.eulerAngles.y, 0.0f));
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    public void Fire()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            print("hit");
+            if(hit.transform.gameObject.tag == "Target")
+            {
+                Debug.Log("hit target");
+            }
+        }
+    }
+}
---
+++ Cyber_Siege/Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private float depth;
+    public float depthSpeed;
+    private float horizontal;
+    public float horizontalSpeed;
+    public float rotSpeed;
+    private Vector3 translation;
+    private Vector3 rotation;
+    public Rigidbody r;
+
+	// Use this for initialization
+	void Start () {
+        r = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            Move();
+        }
+        if(Input.GetAxis("Mouse X") != 0)
+        {
+            Turn();
+        }
+	}
+
+    private void Move()
+    {
+        depth = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        translation.z = depth * Time.deltaTime * depthSpeed;
+        translation.x = horizontal * Time.deltaTime * horizontalSpeed;
+        transform.Translate(translation);
+    }
+
+    private void Turn()
+    {
+        rotation.y += Input.GetAxis("Mouse X") * rotSpeed;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, rotation.y, 0.0f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            translation.y = 0;
+        }
+    }
+}
