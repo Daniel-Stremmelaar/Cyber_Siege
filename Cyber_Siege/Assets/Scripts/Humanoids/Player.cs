0b045f54becd9e445e4780583909803e4bfb838a
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2019-09-02 14:05:32 +0200

    Controller Changes
    Working on a very basic version of vaulting

--- Cyber_Siege/Assets/Scripts/Humanoids/Player.cs
+++ Cyber_Siege/Assets/Scripts/Humanoids/Player.cs
@@ -10,6 +10,17 @@ public class Player : MonoBehaviour
     [SerializeField] float movementSpeedModifier, rotationModifier;
     [SerializeField] float sidewaysWalkDebuff, backwardsWalkDebuff, crouchWalkDebuff;
     [SerializeField] bool crouching;
+    [SerializeField] LayerMask interactableMask;
+    [SerializeField] string vaultableTag;
+    [SerializeField] float vaultDetectionRange;
+    [SerializeField] float vaultSpeed;
+    [SerializeField] States currentState;
+
+    [Header("Sizes")]
+    [SerializeField] Vector3 standColliderSize;
+    [SerializeField] Vector3 standColliderPosition;
+    [SerializeField] Vector3 crouchColliderSize, crouchColliderPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +28,25 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        if(currentState != States.Disabled)
+        {
+            Movement();
+        }
+    }
+    public void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Collider[] vaultables = Physics.OverlapSphere(transform.position, vaultDetectionRange, interactableMask);
+            if(vaultables.Length > 0)
+            {
+                vaultables[0].GetComponent<Interactable>().Interact(gameObject);
+            }
+        }
+    }
+    public void Movement()
     {
         if (crouching)
         {
@@ -25,6 +54,9 @@ public class Player : MonoBehaviour
             {
                 crouching = false;
                 playerAnimator.SetBool("Crouching", crouching);
+                BoxCollider playerCollider = GetComponent<BoxCollider>();
+                playerCollider.center = standColliderPosition;
+                playerCollider.size = standColliderSize;
             }
         }
         else
@@ -33,20 +65,23 @@ public class Player : MonoBehaviour
             {
                 crouching = true;
                 playerAnimator.SetBool("Crouching", crouching);
+                BoxCollider playerCollider = GetComponent<BoxCollider>();
+                playerCollider.center = crouchColliderPosition;
+                playerCollider.size = crouchColliderSize;
             }
         }
         Vector3 movementAmount = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         playerAnimator.SetFloat("SidewaysWalking", movementAmount.x);
         playerAnimator.SetFloat("ForwardWalking", movementAmount.z);
-        if(movementAmount != Vector3.zero)
+        if (movementAmount != Vector3.zero)
         {
-            if(movementAmount.z == 0)
+            if (movementAmount.z == 0)
             {
                 movementAmount *= (1 - (sidewaysWalkDebuff / 100));
             }
             else
             {
-                if(movementAmount.z < 0)
+                if (movementAmount.z < 0)
                 {
                     movementAmount *= (1 - (backwardsWalkDebuff / 100));
                 }
@@ -77,4 +112,23 @@ public class Player : MonoBehaviour
         transform.Rotate(new Vector3(0, cameraRotationAmount.y, 0) * Time.deltaTime * rotationModifier);
         playerCamera.Rotate(new Vector3(cameraRotationAmount.x, 0, 0) * Time.deltaTime * rotationModifier);
     }
+    public IEnumerator Vault(Vector3[] vaultPositions)
+    {
+        currentState = States.Disabled;
+        GetComponent<Collider>().isTrigger = true;
+        GetComponent<Rigidbody>().useGravity = false;
+        for(int i = 0; i < vaultPositions.Length; i++)
+        {
+            while (Vector3.Distance(transform.position, vaultPositions[i]) > 0)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, vaultPositions[i], vaultSpeed * Time.deltaTime);
+                yield return null;
+            }
+        }
+        //yield return new WaitForSeconds(0.1f);
+        currentState = States.Normal;
+        GetComponent<Collider>().isTrigger = false;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+    public enum States { Normal, Disabled, ActionImpaired}
 }
---
+++ Cyber_Siege/Assets/Scripts/Vaultable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vaultable : Interactable
+{
+    [SerializeField] Transform[] vaultPositions;
+    public override void Interact(GameObject player)
+    {
+        List<Vector3> vaultPositionCopy = new List<Vector3>();
+        foreach(Transform pos in vaultPositions)
+        {
+            vaultPositionCopy.Add(pos.position);
+        }
+        if(Vector3.Distance(player.transform.position, vaultPositionCopy[0]) > Vector3.Distance(player.transform.position, vaultPositionCopy[vaultPositionCopy.Count - 1]))
+        {
+            vaultPositionCopy.Reverse();
+        }
+        StartCoroutine(player.GetComponent<Player>().Vault(vaultPositionCopy.ToArray()));
+    }
+}
