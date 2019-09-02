5242cc3210c4705b9fdae98215c828df43f1adf8
MauritsHeetkamp <mauritsmupsketje@gmail.com>
2019-09-02 11:23:20 +0200

    Player Controller
    Movement, Camera.
    Animations make it look kinda strange.
    Add UX to some actions.
    Perhaps an animation instead of code moving the camera? Would give better results.

--- Cyber_Siege/Assets/Scripts/Humanoids/Player.cs
+++ Cyber_Siege/Assets/Scripts/Humanoids/Player.cs
@@ -4,24 +4,77 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Transform playerCamera;
+    [SerializeField] Transform playerCamera, standingCamPos, crouchingCamPos, crouchWalkCamPos, standingWalkCamPos;
+    [SerializeField] float transitionModifier;
     [SerializeField] Animator playerAnimator;
-    [SerializeField] float movementSpeedModifier;
+    [SerializeField] float movementSpeedModifier, rotationModifier;
+    [SerializeField] float sidewaysWalkDebuff, backwardsWalkDebuff, crouchWalkDebuff;
+    [SerializeField] bool crouching;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (crouching)
+        {
+            if (Input.GetButtonUp("Crouch"))
+            {
+                crouching = false;
+                playerAnimator.SetBool("Crouching", crouching);
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Crouch"))
+            {
+                crouching = true;
+                playerAnimator.SetBool("Crouching", crouching);
+            }
+        }
         Vector3 movementAmount = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         playerAnimator.SetFloat("SidewaysWalking", movementAmount.x);
         playerAnimator.SetFloat("ForwardWalking", movementAmount.z);
         if(movementAmount != Vector3.zero)
         {
+            if(movementAmount.z == 0)
+            {
+                movementAmount *= (1 - (sidewaysWalkDebuff / 100));
+            }
+            else
+            {
+                if(movementAmount.z < 0)
+                {
+                    movementAmount *= (1 - (backwardsWalkDebuff / 100));
+                }
+            }
+            if (crouching)
+            {
+                movementAmount *= (1 - (crouchWalkDebuff / 100));
+                playerCamera.position = Vector3.MoveTowards(playerCamera.position, crouchWalkCamPos.position, transitionModifier * Time.deltaTime);
+            }
+            else
+            {
+                playerCamera.position = Vector3.MoveTowards(playerCamera.position, standingWalkCamPos.position, transitionModifier * Time.deltaTime);
+            }
             transform.Translate(movementAmount * movementSpeedModifier * Time.deltaTime);
         }
+        else
+        {
+            if (crouching)
+            {
+                playerCamera.position = Vector3.MoveTowards(playerCamera.position, crouchingCamPos.position, transitionModifier * Time.deltaTime);
+            }
+            else
+            {
+                playerCamera.position = Vector3.MoveTowards(playerCamera.position, standingCamPos.position, transitionModifier * Time.deltaTime);
+            }
+        }
+        Vector2 cameraRotationAmount = new Vector2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+        transform.Rotate(new Vector3(0, cameraRotationAmount.y, 0) * Time.deltaTime * rotationModifier);
+        playerCamera.Rotate(new Vector3(cameraRotationAmount.x, 0, 0) * Time.deltaTime * rotationModifier);
     }
 }
