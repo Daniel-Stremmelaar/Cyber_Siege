7c99b606ffdde49a2d921540accf80dbdb8fade2
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2019-10-20 23:11:24 +0200

    Tutorial arrow move
    Arrows now move in the tutorial.

---
+++ Cyber_Siege/Assets/Scripts/Tutorial/TutorialArrow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialArrow : MonoBehaviour
+{
+    public float distance;
+    public float speed;
+    private float traveled;
+    private bool forward;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(traveled > distance)
+        {
+            forward = false;
+        }
+        if(traveled < -distance)
+        {
+            forward = true;
+        }
+
+        if (forward)
+        {
+            gameObject.transform.Translate(-Vector3.right * Time.deltaTime * speed);
+            traveled += Time.deltaTime;
+        }
+        else
+        {
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * speed);
+            traveled -= Time.deltaTime;
+        }
+        
+    }
+}
