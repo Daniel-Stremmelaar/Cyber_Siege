a2cae64f3af72ed5fb49332d8be60747f3b3bc8d
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2019-10-30 11:40:53 +0100

    push

---
+++ Cyber_Siege/Assets/Scripts/Lights.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    public Light[] lightsList;
+    float distance;
+    public float rightAmount;
+
+    void Start()
+    {
+        // find all objects with lights
+        lightsList = FindObjectsOfType(typeof(Light)) as Light[];
+        foreach (Light light in lightsList)
+        {
+            light.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < lightsList.Length; i++)
+        {
+            Light test = lightsList[i];
+            distance = Vector3.Distance(transform.position, test.transform.position);
+            if (distance < rightAmount)
+            {
+                test.enabled = true;
+            }
+            else
+            {
+                test.enabled = false;
+            }
+        }
+    }
+}
