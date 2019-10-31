fa158b5821cc5ba4c38d32d30993dfe490b7ae8e
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2019-10-31 15:37:50 +0100

    Grenade explosion & Pause menu
    Explosion needs new sound. Pause menu does not display in killhouse.

---
+++ Cyber_Siege/Assets/Scripts/ExplosionTester.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionTester : MonoBehaviour
+{
+    public AudioClip boom;
+    public AudioSource speaker;
+    public ParticleSystem fire;
+    public ParticleSystem smoke;
+    public ParticleSystem debris;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            speaker.PlayOneShot(boom);
+            fire.Play();
+            smoke.Play();
+            debris.Play();
+        }
+    }
+}
